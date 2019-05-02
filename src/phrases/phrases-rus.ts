4d58d92c505320f56c337b36ca5a88b831e13f86
Max Zinovik <zinovik@gmail.com>
2019-05-02 18:07:24 +0300

    update polling

--- src/phrases/phrases-rus.ts
+++ src/phrases/phrases-rus.ts
@@ -69,3 +69,40 @@ export const DAYS_OF_WEEK = [
 export const NEW_SCHEDULE = 'Новое расписание:';
 
 export const CHANGES = 'Изменения:';
+
+export const DAYS_OF_WEEK_BUTTONS = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        {
+          text: 'ПН',
+          callback_data: 'monday',
+        },
+        {
+          text: 'ВТ',
+          callback_data: 'tuesday',
+        },
+        {
+          text: 'СР',
+          callback_data: 'wednesday',
+        },
+        {
+          text: 'ЧТ',
+          callback_data: 'thursday',
+        },
+        {
+          text: 'ПТ',
+          callback_data: 'friday',
+        },
+        {
+          text: 'СБ',
+          callback_data: 'saturday',
+        },
+        {
+          text: 'ВС',
+          callback_data: 'sunday',
+        },
+      ],
+    ],
+  },
+};
