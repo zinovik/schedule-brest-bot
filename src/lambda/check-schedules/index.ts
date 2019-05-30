13bf746ff1377f0ba8b9833bd089c6c4e79ea1e5
Max Zinovik <zinovik@gmail.com>
2019-05-30 14:31:51 +0300

    add check-schedules error

--- src/lambda/check-schedules/index.ts
+++ src/lambda/check-schedules/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
+import * as https from 'https';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
-import axios from 'axios';
 
 import {
   schedulerIce,
@@ -12,15 +12,41 @@ dotenv.config();
 
 const TELEGRAM_API_URL = 'https://api.telegram.org/bot';
 
+const makeApiRequest = (url: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (resp) => {
+      let data = '';
+
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        resolve(data);
+      });
+
+    }).on('error', (error) => {
+      reject(error);
+    });
+  });
+};
+
 const bot = {
   sendMessage: async (channelId: string, text: string): Promise<void> => {
-    await axios.get(encodeURI(`${TELEGRAM_API_URL}${process.env.TOKEN}/sendMessage?chat_id=${channelId}&text=${text}`));
+    await makeApiRequest(encodeURI(`${TELEGRAM_API_URL}${process.env.TOKEN}/sendMessage?chat_id=${channelId}&text=${text}`));
   },
 };
 
 const handler = async (_: IncomingMessage, res: ServerResponse) => {
-  await schedulerIce(bot);
-  await schedulerDvvs(bot);
+  try {
+    await schedulerIce(bot);
+    await schedulerDvvs(bot);
+  } catch (error) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      error,
+    }));
+  }
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
