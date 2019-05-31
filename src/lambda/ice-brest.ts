d48ef091c8a476626fcdfcc376126267f7fd5eb4
Max Zinovik <zinovik@gmail.com>
2019-05-31 17:18:11 +0300

    fix async functions

--- src/lambda/ice-brest.ts
+++ src/lambda/ice-brest.ts
@@ -1,23 +1,25 @@
-const dotenv = require('dotenv');
+import 'babel-polyfill';
+import * as dotenv from 'dotenv';
 
-const { getScheduleDb, SCHEDULE_ICE } = require('../db');
+import { getScheduleDb, SCHEDULE_ICE } from '../db';
 
 dotenv.config();
 
-exports.handler = (event: any, context: any, callback: any) => {
-  getScheduleDb(SCHEDULE_ICE)
-    .then((scheduleIceDb: string) => {
-      callback(null, {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: scheduleIceDb,
-      });
-    })
-    .catch(() => {
-      callback(null, {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: 'Something went wrong...',
-      });
-    });
+exports.handler = async (event: any, context: any) => {
+  try {
+    const scheduleIceDb = await getScheduleDb(SCHEDULE_ICE);
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: scheduleIceDb,
+    };
+  } catch (error) {
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: 'Something went wrong...',
+    };
+  }
 };
