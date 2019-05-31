4220ce729b9bb1a4eb735833925128b2f4d44a24
Max Zinovik <zinovik@gmail.com>
2019-05-31 14:10:36 +0300

    remove unnecessary modules

--- webpack.functions.js
+++ webpack.functions.js
@@ -4,7 +4,9 @@ module.exports = {
   resolve: {
     alias: {
       'canvas': path.join(__dirname, '/empty-module.js'),
-      'hiredis': path.join(__dirname, '/empty-module.js')
+      'hiredis': path.join(__dirname, '/empty-module.js'),
+      'utf-8-validate': path.join(__dirname, '/empty-module.js'),
+      'bufferutil': path.join(__dirname, '/empty-module.js')
     }
   }
 };
